b7d1d2f3e6cc39bbba6582ec6800bb3409506874
Kota Kato <peony.btn@gmail.com>
2022-09-09 00:15:20 +0900

    [awk1] Define AWKFields

--- awk1/src/main.rs
+++ awk1/src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::io::{self, Error};
 
 mod ast;
 
@@ -11,13 +11,24 @@ struct AWKFields {
     fields: Vec<String>,
 }
 
+impl AWKFields {
+    fn nf(&self) -> usize {
+        self.fields.len()
+    }
+}
+
 fn mainloop() {
     loop {
         let mut line = String::new();
-        if io::stdin().read_line(&mut line).expect("Failed to read line.") != 0 {
+        if io::stdin()
+            .read_line(&mut line)
+            .expect("Failed to read line.")
+            != 0
+        {
             let fields = AWKFields {
                 fields: line.trim().split(" ").map(|f| f.to_string()).collect(),
             };
+            dbg!(fields.nf());
             dbg!(&fields);
         } else {
             break;

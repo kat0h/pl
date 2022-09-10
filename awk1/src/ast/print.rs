2079fbe7e52e1396edfa778e3b50eec4c61e4545
Kota Kato <peony.btn@gmail.com>
2022-09-10 19:13:57 +0900

    [awk1] can parse `print`

--- awk1/src/ast/print.rs
+++ awk1/src/ast/print.rs
@@ -1,6 +1,11 @@
 use nom::{
-    branch::permutation, bytes::complete::tag, character::complete::char, combinator::map,
-    multi::many0, sequence::delimited, IResult,
+    branch::permutation,
+    bytes::complete::tag,
+    character::complete::char,
+    combinator::{map, opt},
+    multi::many0,
+    sequence::delimited,
+    IResult,
 };
 
 use crate::ast::{def::AWKPrint, expr::parse_expr};
@@ -34,7 +39,15 @@ pub fn parse_print(input: &str) -> IResult<&str, AWKPrint> {
     // print(Expr,Expr,Expr)
     let (input, (_, exprlist)) = permutation((
         tag("print"),
-        delimited(char('('), parse_print_expr_list, char(')')),
+        map(
+            opt(delimited(char('('), parse_print_expr_list, char(')'))),
+            |expr: Option<Vec<AWKExpr>>| -> Vec<AWKExpr> {
+                match expr {
+                    Some(expr) => expr,
+                    None => vec![],
+                }
+            },
+        ),
     ))(input)?;
 
     Ok((input, AWKPrint { exprlist }))
@@ -48,4 +61,26 @@ fn test_parse_print_expr_list() {
     ];
     let a = parse_print_expr_list("123,\"hoge\"").unwrap().1;
     assert_eq!(e, a);
+}
+
+#[test]
+fn test_parse_print() {
+    assert_eq!(
+        Ok(("", AWKPrint {
+            exprlist: parse_print_expr_list("123,\"456\"").unwrap().1
+        })),
+        parse_print("print(123,\"456\")")
+    );
+    assert_eq!(
+        Ok(("", AWKPrint {
+            exprlist: vec![]
+        })),
+        parse_print("print")
+    );
+    assert_eq!(
+        Ok(("()", AWKPrint {
+            exprlist: vec![]
+        })),
+        parse_print("print()")
+    );
 }

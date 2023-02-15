d95050319f5a6cc665a6bdda4027c959c05df2ae
Kota Kato <45391880+kat0h@users.noreply.github.com>
2023-02-15 23:14:33 +0900

    [basic-interpreter] WIP

--- basic-interpreter/src/main.rs
+++ basic-interpreter/src/main.rs
@@ -1,6 +1,7 @@
 /*
  * Basic Interpriter
  */
+use std::io;
 
 fn main() {
     mainloop();
@@ -8,7 +9,29 @@ fn main() {
 
 fn mainloop() {
     let mut line = String::new();
-    io::stdin().read_line(&mut line).expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut line)
+        .expect("Failed to read line");
 
     println!("{}", line);
+}
+
+peg::parser! {
+  grammar parse_line() for str {
+    rule print() -> STMTPrint
+      = "print" n:$['0'..='9'] { STMTPrint(n.parse().or(Err("print arg error")) }
+
+    pub rule line() -> STMT
+      = n:print() { STMT::Print(n) }
+  }
+}
+
+#[derive(Debug, PartialEq)]
+enum STMT {
+    Print(STMTPrint)
+}
+
+#[derive(Debug, PartialEq)]
+struct STMTPrint {
+    items: Vec<i64>,
 }

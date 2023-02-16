2d00b8d1d70817073510acd048f668b74eabb148
Kota Kato <45391880+kat0h@users.noreply.github.com>
2023-02-16 23:14:08 +0900

    [basic-interpreter] Print Function and Number

--- basic-interpreter/src/main.rs
+++ basic-interpreter/src/main.rs
@@ -8,30 +8,52 @@ fn main() {
 }
 
 fn mainloop() {
-    let mut line = String::new();
-    io::stdin()
-        .read_line(&mut line)
-        .expect("Failed to read line");
+    loop {
+        let mut line = String::new();
+        io::stdin()
+            .read_line(&mut line)
+            .expect("Failed to read line");
 
-    println!("{}", line);
+        //parse line
+        let parsed = parse_line::line(&line);
+        match parsed {
+            Ok(stmt) => {
+                match stmt {
+                    Stmt::Print(val) => {
+                        for v in val.items.iter() {
+                            println!("{}", v);
+                        }
+                    }
+                }
+            },
+            Err(_) => {
+                println!("Syntax Error!");
+            }
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Stmt {
+    Print(StmtPrint),
+}
+
+#[derive(Debug, PartialEq)]
+pub struct StmtPrint {
+    items: Vec<i64>,
 }
 
 peg::parser! {
   grammar parse_line() for str {
-    rule print() -> STMTPrint
-      = "print" n:$['0'..='9'] { STMTPrint(n.parse().or(Err("print arg error")) }
+    rule _() = [' ' | '\t']*
 
-    pub rule line() -> STMT
-      = n:print() { STMT::Print(n) }
+    rule number() -> i64
+      = n:$(['0'..='9']+) {? n.parse::<i64>().or(Err("i64")) }
+
+    rule print() -> StmtPrint
+      = "print" _ n:number() { StmtPrint{ items: vec![n] }}
+
+    pub rule line() -> Stmt
+      = n:print() "\n" { Stmt::Print(n) }
   }
-}
-
-#[derive(Debug, PartialEq)]
-enum STMT {
-    Print(STMTPrint)
-}
-
-#[derive(Debug, PartialEq)]
-struct STMTPrint {
-    items: Vec<i64>,
 }

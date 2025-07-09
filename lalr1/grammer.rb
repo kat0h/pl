6126b4f7f9b9f5677c1aeba455e7827f8abcc6dd
Kota Kato <45391880+kat0h@users.noreply.github.com>
2025-07-09 11:17:07 +0900

    [lalr1] feat: Convert Grammer and Rule to class

--- lalr1/grammer.rb
+++ lalr1/grammer.rb
@@ -1,20 +1,35 @@
 require_relative "lr_n"
 
-Grammer = Struct.new :vn, :vt, :s, :p, :precedence, keyword_init: true do
+class Grammer
+  attr_reader :vn, :vt, :s, :p, :precedence
+  def initialize(vn:, vt:, s:, p:, precedence:)
+    @vn = vn
+    @vt = vt
+    @s = s
+    @p = p
+    @precedence = precedence
+  end
   def inspect
-    ret = "{\n  Vn: {#{vn.to_a.map(&:to_s).join(", ")}}\n"
+    ret = "{\nVn: {#{vn.to_a.map(&:to_s).join(", ")}}\n"
     ret << "  Vt: {#{vt.to_a.map(&:to_s).join(", ")}}\n"
     ret << "  S: #{s},\n"
     ret << "  P: {\n"
-    p.each do
-      ret << "    " << it.inspect << ",\n"
-    end
+    p.each do |it| ret << "    " << it.inspect << ",\n" end
     ret << "  }\n"
     ret << "  precedence: #{precedence}\n"
     ret << "}"
   end
 end
-Rule = Struct.new(:l, :r, :act) do
-  def inspect = l.to_s << " → " << r.join(" ")
+
+class Rule
+  attr_reader :l, :r, :act
+  def initialize(l, r, act)
+    @l = l
+    @r = r
+    @act = act
+  end
+  def inspect = "#{l} → #{r.join(" ")}"
   def to_lr1(dot, ls) = LR1.new(l, r, dot, ls, act)
+  def eql?(other) = self.class == other.class && l == other.l && r == other.r
+  def hash = [l, r].hash
 end

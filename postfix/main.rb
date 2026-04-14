c7eabfd212d1d487f909d2c10520f24201391dac
Kota Kato <45391880+kat0h@users.noreply.github.com>
2026-04-14 15:54:08 +0900

    [postfix] テストケースを追加

--- postfix/main.rb
+++ postfix/main.rb
@@ -4,7 +4,7 @@
 
 # Commands
 # N
-# add sub div mul rem 
+# add sub div mul rem
 # lt gt eq
 # pop
 # swap
@@ -13,67 +13,96 @@
 # (C1 ... Cn)
 # exec
 
-def eval(p, args)
-  argc = p[1]
+def eval_postfix(p, args)
   cont = p[2..]
+  return :error if args.size != p[1]
+
   stack = args.reverse
-  while op = cont.shift
+  while (op = cont.shift)
     case op
     when Integer
       stack.push op
     when Array
       stack.push op
     when :add
+      (return :error) if stack.size < 2
       v1 = stack.pop
       v2 = stack.pop
+      (return :error) if (!v1 in Integer) || (!v2 in Integer)
       stack.push v2 + v1
     when :sub
+      (return :error) if stack.size < 2
       v1 = stack.pop
       v2 = stack.pop
+      (return :error) if (!v1 in Integer) || (!v2 in Integer)
       stack.push v2 - v1
     when :mul
+      (return :error) if stack.size < 2
       v1 = stack.pop
       v2 = stack.pop
+      (return :error) if (!v1 in Integer) || (!v2 in Integer)
       stack.push v2 * v1
     when :div
+      (return :error) if stack.size < 2
       v1 = stack.pop
       v2 = stack.pop
-      stack.push v2 / v1
+      (return :error) if !v1.is_a?(Integer) || !v2.is_a?(Integer) || v1.zero?
+      # n = qd + r において、q = truncate(n/d) となる挙動
+      stack.push (v2 / v1.to_f).truncate
+
     when :rem
+      (return :error) if stack.size < 2
       v1 = stack.pop
       v2 = stack.pop
-      stack.push v2 % v1
+      (return :error) if !v1.is_a?(Integer) || !v2.is_a?(Integer) || v1.zero?
+      # 被除数 v2 の符号に合わせる余り (remainder)
+      stack.push v2.remainder(v1)
     when :lt
+      (return :error) if stack.size < 2
       v1 = stack.pop
       v2 = stack.pop
+      (return :error) if (!v1 in Integer) || (!v2 in Integer)
       stack.push v2 < v1 ? 1 : 0
     when :gt
+      (return :error) if stack.size < 2
       v1 = stack.pop
       v2 = stack.pop
+      (return :error) if (!v1 in Integer) || (!v2 in Integer)
       stack.push v2 > v1 ? 1 : 0
     when :eq
+      (return :error) if stack.size < 2
       v1 = stack.pop
       v2 = stack.pop
+      (return :error) if (!v1 in Integer) || (!v2 in Integer)
       stack.push v2 == v1 ? 1 : 0
     when :pop
+      (return :error) if stack.empty?
       stack.pop
     when :swap
+      (return :error) if stack.size < 2
       v1 = stack.pop
       v2 = stack.pop
       stack.push v1
       stack.push v2
     when :sel
+      (return :error) if stack.size < 3
       v1 = stack.pop
       v2 = stack.pop
       v3 = stack.pop
+      (return :error) unless v3 in Integer
       stack.push v3 != 0 ? v2 : v1
     when :nget
       idx = stack.pop
+      (return :error) if (!idx in Integer) || (0 >= idx) || (idx > stack.size)
       stack.push stack[stack.size - idx]
     when :exec
-      cont.unshift *stack.pop
+      (return :error) if stack.empty?
+      seq = stack.pop
+      (return :error) unless seq in Array
+      cont.unshift(*seq)
     end
   end
+  (return :error) if stack.empty? || !stack.last in Integer
   stack.last
 end
 
@@ -91,12 +120,4 @@ def swap = :swap
 def sel = :sel
 def nget = :nget
 def exec = :exec
-
-# given a, b, c, x, calculates ax^2 + bx + c pp.12
-# P = [:postfix, 4, 4, :nget, 5, :nget, :mul, :mul, :swap, 4, :nget, :mul, :add, :add]
-# args = [3,4,5,2]
-
-# an absolute value program pp.12
-# P = [postfix, 1, 1, nget, 0, lt, [0, swap, sub], [], sel, exec]
-# args = [-7]
-p eval(P, args)
+def error = :error
---
+++ postfix/test.rb
@@ -0,0 +1,125 @@
+require_relative './main'
+
+# given a, b, c, x, calculates ax^2 + bx + c pp.12
+# P = [postfix, 4, 4, nget, 5, nget, mul, mul, swap, 4, nget, mul, add, add]
+# args = [3, 4, 5, 2]
+# an absolute value program pp.12
+# P = [postfix, 1, 1, nget, 0, lt, [0, swap, sub], [], sel, exec]
+# args = [-7]
+
+test_cases = [
+  # pp.9
+  {
+    p: [postfix, 0, 1, 2, 3],
+    args: [],
+    v_expected: 3
+  }, {
+    p: [postfix, 0, 1, 2, 3, pop],
+    args: [],
+    v_expected: 2
+  }, {
+    p: [postfix, 0, 1, 2, swap, 3, pop],
+    args: [],
+    v_expected: 1
+  }, {
+    p: [postfix, 0, 1, swap],
+    args: [],
+    v_expected: error
+  }, {
+    p: [postfix, 0, 1, pop, pop],
+    args: [],
+    v_expected: error
+  },
+  # pp.10 (上部の例)
+  {
+    p: [postfix, 2],
+    args: [3, 4],
+    v_expected: 3
+  }, {
+    p: [postfix, 2, swap],
+    args: [3, 4],
+    v_expected: 4
+  }, {
+    p: [postfix, 3, pop, swap],
+    args: [3, 4, 5],
+    v_expected: 5
+  },
+  # pp.11 (Semantics 1.4.2)
+  {
+    p: [postfix, 2, swap],
+    args: [3],
+    v_expected: error # Wrong number of arguments.
+  }, {
+    p: [postfix, 1, pop],
+    args: [4, 5],
+    v_expected: error # Wrong number of arguments.
+  }, {
+    p: [postfix, 1, 4, sub],
+    args: [3],
+    v_expected: -1
+  }, {
+    p: [postfix, 1, 4, add, 5, mul, 6, sub, 7, div],
+    args: [3],
+    v_expected: 4
+  }, {
+    p: [postfix, 5, add, mul, sub, swap, div],
+    args: [7, 6, 5, 4, 3],
+    v_expected: -20
+  }, {
+    p: [postfix, 3, 4000, swap, pop, add],
+    args: [300, 20, 1],
+    v_expected: 4020
+  }, {
+    p: [postfix, 2, add, 2, div],
+    args: [3, 7],
+    v_expected: 5
+  }, {
+    p: [postfix, 1, 3, div],
+    args: [17],
+    v_expected: 5
+  }, {
+    p: [postfix, 1, 3, rem],
+    args: [17],
+    v_expected: 2
+  }, {
+    p: [postfix, 1, 4, lt],
+    args: [3],
+    v_expected: 1
+  }, {
+    p: [postfix, 1, 4, lt],
+    args: [5],
+    v_expected: 0
+  }, {
+    p: [postfix, 1, 4, lt, 10, add],
+    args: [3],
+    v_expected: 11
+  }, {
+    p: [postfix, 1, 4, mul, add],
+    args: [3],
+    v_expected: error # Not enough numbers to add.
+  }, {
+    p: [postfix, 2, 4, sub, div],
+    args: [4, 5],
+    v_expected: error # Divide by zero.
+  }
+]
+
+test_cases.each_with_index do |c, i|
+  p = c[:p]
+  args = c[:args]
+  v_expected = c[:v_expected]
+  v_actual = eval_postfix(p, args)
+  print "Test case #{i + 1}"
+  if v_actual == v_expected
+    puts ' Passed'
+    next
+  else
+    puts ' Failed'
+  end
+
+  puts "P:          #{p}"
+  puts "args:       #{args}"
+  puts "v_expected: #{v_expected}"
+  puts "v_actual:   #{v_actual}"
+  throw 'assert equal'
+end

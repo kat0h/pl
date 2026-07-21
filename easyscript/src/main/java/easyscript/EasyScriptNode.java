2e9c98f1b790bcb65405f49c92fbea0610cf60ab
Kota Kato <45391880+kat0h@users.noreply.github.com>
2026-07-21 16:03:53 +0900

    [easyscript] 環境構築

---
+++ easyscript/src/main/java/easyscript/EasyScriptNode.java
@@ -0,0 +1,6 @@
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.Node;
+
+public abstract class EasyScriptNode extends Node {
+  public abstract int executeInt(VirtualFrame frame);
+}

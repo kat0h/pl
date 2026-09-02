ab607831935dbd89aacabdfb6cd4c187811f6456
Kota Kato <45391880+kat0h@users.noreply.github.com>
2026-09-02 17:05:08 +0900

    [easyscript] add test

--- easyscript/src/test/java/easyscript/GlobalVarTest.java
+++ easyscript/src/test/java/easyscript/GlobalVarTest.java
@@ -1,12 +1,43 @@
 package easyscript;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Set;
+
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Value;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GlobalVarTest {
+  private Context context;
+  @BeforeEach
+  void setUp() {
+    this.context = Context.create();
+  }
+  @AfterEach
+  void tearDown() {
+    this.context.close();
+  }
   @Test
-  void voidtest() {
-    assertEquals(1, 1);
+  void test1() {
+    Value result = this.context.eval("ezs", "" +
+        "var a = 1; " +
+        "let b = 2 + 3; " +
+        "const c = 4 + 5.0; " +
+        "(a = a + b + a) + a"
+    );
+
+    assertEquals(14, result.asInt());
+    Value globalBindings = this.context.getBindings("ezs");
+    assertFalse(globalBindings.isNull());
+    assertTrue(globalBindings.hasMembers());
+    assertTrue(globalBindings.hasMember("a"));
+    Value a = globalBindings.getMember("a");
+    assertEquals(7, a.asInt());
+    assertEquals(Set.of("a", "b", "c"), globalBindings.getMemberKeys());
   }
 }
